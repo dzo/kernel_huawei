a346cdbfbf9af15f3d7dbe6e62bf8fbe2e4fe8d4
Paul <paulmilbank@gmail.com>
2012-10-14 21:45:34 +1300

    Up Default Lowmemorykiller values

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -47,10 +47,10 @@ static int lowmem_adj[6] = {
 };
 static int lowmem_adj_size = 4;
 static size_t lowmem_minfree[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	16 * 1024,	/* 64MB */
+	4 * 512,	/* 6MB - PaulM 8MB */
+	4 * 1024,	/* 8MB - PaulM 16MB */ 
+	8 * 1024,	/* 16MB - PaulM 32MB */
+	24 * 1024,	/* 64MB - PaulM 96MB */
 };
 static int lowmem_minfree_size = 4;
 
